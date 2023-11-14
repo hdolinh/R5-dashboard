5721712ca41f74fbd505318a0537e76d81605677
hdolinh <halina@bren.ucsb.edu>
2023-11-13 17:59:25 -0800

    crombined 2018 and up data processing steps

--- scripts/ridb-processing.R
+++ scripts/ridb-processing.R
@@ -25,18 +25,19 @@ raw_ridb2020 <- vroom(fp_ridb2020)
 fp_ridb2021 <- here("data/ridb/raw/FY21 Historical Reservations Full.csv")
 raw_ridb2021 <- vroom(fp_ridb2021)
 
-# 2018 clean and subset ridb ----
-usfs_ridb2018 <- raw_ridb2018 %>% 
-  # add sept_out to rm `_` to match other column names
+# combined processing ----
+usfs_ridb <- raw_ridb2021 %>% 
+  # add sept_out to rm `_` to match column names
   janitor::clean_names(sep_out = "") %>% 
-  # cols of interest
+  
+  ## subset data ##
+  # 1. cols of interest
   select(
     historicalreservationid,
     agency,
     parentlocation,
     park,
     sitetype,
-    usetype,
     facilityzip,
     facilitystate,
     facilitylongitude,
@@ -48,27 +49,35 @@ usfs_ridb2018 <- raw_ridb2018 %>%
     orderdate,
     numberofpeople
   ) %>% 
-  # extract 5 digit zip codes 
+  # 2. filter for just USFS in CA
+  # NOTEHD: `case_when()` is for 2018 data
+  mutate(facilitystate = case_when(facilitystate == "CA" ~ "California",
+                                   TRUE ~ facilitystate)) %>% 
+  filter(agency == "USFS" & facilitystate == "California") %>% 
+  
+  ## clean customer zip codes ##
+  # 1. extract 5 digit customer zip codes 
   mutate(customerzip = str_extract(string = customerzip,
                                    pattern = "[:digit:]{5}")) %>% 
-  # rm invalid zip codes
-  filter(!customerzip %in% c("00000", "99999")) %>% 
-  # filter for just USFS in CA, OR, and WA
-  filter(agency == "USFS" & facilitystate %in% c("CA", "OR", "WA")) %>% 
-  # rename parent_location to forestname
+  # 2. rm invalid customer zip codes
+  filter(!customerzip %in% c("00000", "99999")) %>%
+  # 3. rm NA customer zip codes
+  drop_na(customerzip) %>% 
+  
+  ## clean forestname ##
+  # 1. rename parentlocation to forestname
   rename(forestname = parentlocation) %>% 
-  # clean forestname #
   mutate(
-    # standardize "National Forest"
+    # 2. standardize "National Forest"
     forestname = str_replace(string = forestname,
                              pattern = paste(c(
                                "NF - FS", "NF -FS", "NF- FS",
                                "NF-FS", "-FS", " - FS"),
                                collapse = "|"),
                              replacement = "National Forest"),
-    # standardize to title case
+    # 3. standardize to title case
     forestname = str_to_title(forestname),
-    # fix forestname typos based on spatial data
+    # 4. fix forestname typos based on spatial data
     forestname = case_when(
       forestname == "Columbia R Gorge Nsanational Forest" ~ "Columbia River Gorge National Scenic Area",
       forestname == "Fremont Winema National Forest" ~ "Fremont-Winema National Forest",
@@ -79,15 +88,19 @@ usfs_ridb2018 <- raw_ridb2018 %>%
       # keep forestname if none of the previous conditions are TRUE
       TRUE ~ forestname)
   ) %>% 
-  # rm forests not in R5; forestname var comes from spatial data script
+  # 5. rm forests NOT in R5
+  # NOTEHD: forestname var comes from spatial data script
   filter(forestname %in% forestname_R5) %>% 
-  # clean park #
+  
+  ## clean park ##
   mutate(
+    # 1. standardize case
     park = str_to_title(park), # 367 tot
-    # string errors
+    # 2. fix string errors
     park = str_remove(string = park,
                       pattern = paste(c("\\(.*", " \\(.*",
                                         "---.*", " ---.*",
+                                        "-.*", " -.*",
                                         ",.*"), collapse = "|")),
     park = str_replace(string = park,
                        pattern = "@",
@@ -96,17 +109,17 @@ usfs_ridb2018 <- raw_ridb2018 %>%
                        pattern = "Cg",
                        replacement = "Campground"),
     park = str_replace(string = park,
+                       pattern = "&",
+                       replacement = "And"),
+    park = str_replace(string = park,
                        pattern = paste(c("/", " / "), collapse = "|"),
                        replacement = " "),
-    # match park with 2019+ data
+    # 3. match park values for each NF
     park = case_when(
       # Angeles NF
-      park == "Pyramid Lake / Los Alamos Campground" ~ "Pyramid Lake - Los Alamos Campground",
-      park == "Table Mountain - Angeles Nf" ~ "Table Mountain",
+      park == "Pyramid Lake" ~ "Pyramid Lake Los Alamos Campground",
       # Eldorado NF
       park == "Bear Group Campground" ~ "Bear River Group Campground",
-      park == "Silver Lake East" ~ "Silver Lake East- Eldorado",
-      park == "South Fork Group" ~ "South Fork Group (Ca)",
       # Inyo NF
       park == "Silver Lake Campground" ~ "Silver Lake Campground June Lake",
       park == "Table Mountain Inyo" ~ "Table Mountain",
@@ -114,7 +127,7 @@ usfs_ridb2018 <- raw_ridb2018 %>%
       park == "Arroyo Seco Campground" ~ "Arroyo Seco",
       park == "Los Prietos Campground" ~ "Los Prietos",
       # Plumas NF
-      park == "Cottonwood\nThousand Trails Management Services, Inc" ~ "Cottonwood",
+      park == "Cottonwood Thousand Trails Management Services, Inc" ~ "Cottonwood",
       # San Bernardino NF
       park == "Black Mountain Group" ~ "Black Mountain",
       # Sequoia NF
@@ -126,102 +139,22 @@ usfs_ridb2018 <- raw_ridb2018 %>%
       # Stanislaus NF
       park == "Lodgepole/Bear Valley" ~ "Lodgepole Group",
       park == "Pioneer Trails" ~ "Pioneer Trail",
+      park == "Big Meadow Stanislaus Natl Fs" ~ "Big Meadow",
+      park == "Lake Alpine - Lodgepole Group" ~ "Lodgepole Group",
       # Tahoe NF
       park == "Boca Rest" ~ "Boca Rest Campground",
       park == "Calpine Lookout Cabin" ~ "Calpine Lookout",
       park == "Sierra Campground" ~ "Sierra",
       park == "Tunnel Mills Il" ~ "Tunnel Mills Group",
-      TRUE ~ park
-    ),
-    # calculate new variables
-    lengthofstay = as.numeric(difftime(enddate, startdate), units = "days"), 
-    bookingwindow = round(as.numeric(difftime(startdate, orderdate), units = "days"), 0),
-    dailycost = round(totalpaid / lengthofstay, 2),
-    dailycostpervisitor = round(dailycost / numberofpeople, 2),
-    # convert sitetype to title case
-    sitetype = str_to_title(sitetype)
-  )
-
-test2018 <- usfs_ridb2018 %>% 
-  filter(sitetype == "Yurt") %>% 
-  count(park)
-
-  
-# 2019-2021 clean and subset ridb ----
-usfs_ridb <- raw_ridb2021 %>% 
-  # match colnames
-  janitor::clean_names(sep_out = "") %>% 
-  # cols of interest
-  select(
-    historicalreservationid,
-    agency,
-    parentlocation,
-    park,
-    sitetype,
-    usetype,
-    facilityzip,
-    facilitystate,
-    facilitylongitude,
-    facilitylatitude,
-    customerzip,
-    totalpaid,
-    startdate,
-    enddate,
-    orderdate,
-    numberofpeople
-  ) %>% 
-  # extract 5 digit zip codes 
-  mutate(customerzip = str_extract(string = customerzip,
-                                   pattern = "[:digit:]{5}")) %>% 
-  # rm invalid zip codes
-  filter(!customerzip %in% c("00000", "99999")) %>% 
-  # filter for just USFS in CA, OR, and WA
-  filter(agency == "USFS" & facilitystate %in% c("California",
-                                                 "Oregon",
-                                                 "Washington")) %>% 
-  # rename parent_location to forestname
-  rename(forestname = parentlocation) %>% 
-  # clean forestname
-  mutate(
-    forestname = case_when(forestname == "SHASTA-TRINITY NATIONAL FOREST" ~ "Shasta-Trinity National Forest",
-                           TRUE ~ forestname)
-  ) %>%
-  # rm forests not in R5; forestname_R5 var comes from spatial data script
-  filter(forestname %in% forestname_R5) %>% 
-  # clean park #
-  mutate(
-    park = str_to_title(park), # 381 tot (2019) 385 tot (2020) 402 tot (2021)
-    # string errors
-    park = str_remove(string = park,
-                      pattern = paste(c("\\(.*", " \\(.*", 
-                                        "-.*", " -.*"), 
-                                      collapse = "|")),
-    park = str_replace(string = park,
-                       pattern = "Cg",
-                       replacement = "Campground"),
-    park = str_replace(string = park,
-                       pattern = "@",
-                       replacement = "At"),
-    park = str_replace(string = park,
-                       pattern = "&",
-                       replacement = "And"),
-    park = str_replace(string = park,
-                       pattern = paste(c("/", " / "), collapse = "|"),
-                       replacement = " "),
-    # match park with 2018 data
-    park = case_when(
-      # Stanislaus NF
-      park == "Big Meadow Stanislaus Natl Fs" ~ "Big Meadow",
-      park == "Lake Alpine - Lodgepole Group" ~ "Lodgepole Group",
-      # Tahoe NF
       park == "Tunnel Mills Ii" ~ "Tunnel Mills Group",
       TRUE ~ park
     ),
-    # convert to character to reveal full coordinate
+    # 4. fill in missing park values 
+    # a. convert to char to reveal full coordinate
     facilitylongitude = as.character(facilitylongitude),
     facilitylatitude = as.character(facilitylatitude),
+    # b. use lat/long to fill in missing park values
     park = case_when(
-      # fill in missing data based on lat/long
       facilitylatitude == "38.477308" & facilitylongitude == "-120.024175" ~ "Lodgepole Overflow Campground",
       facilitylatitude == "38.4811" & facilitylongitude == "-120.017" ~ "Silvertip Campground",
       facilitylatitude == "38.480752" & facilitylongitude == "-119.988643" ~ "Pine Marten Campground", # 2021 data
@@ -230,26 +163,36 @@ usfs_ridb <- raw_ridb2021 %>%
       facilitylatitude == "38.477333" & facilitylongitude == "-120.008045" ~ "Lake Alpine West Shore Campground",
       TRUE ~ park
     ),
-    # convert back to numeric
+    # c. convert lat/long back to numeric
     facilitylongitude = as.numeric(facilitylongitude),
-    facilitylatitude = as.numeric(facilitylatitude),
-    # calculate new variables #
-    # length of stay
-    lengthofstay = as.numeric(difftime(enddate, startdate), units = "days"),
-    # change neg length of stay to positive;
+    facilitylatitude = as.numeric(facilitylatitude)
+    ) %>% 
+  
+  ## calculate new variables ##
+  mutate(
+    # 1. length of stay
+    lengthofstay = as.numeric(difftime(enddate, startdate), units = "days"), 
+    # a. change neg length of stay to positive;
     # NOTEHD: previous conversations w/ R1S confirm neg start date is data error
     lengthofstay = if_else(lengthofstay < 0,
                            true = as.numeric(difftime(startdate, enddate), units = "days"),
                            false = lengthofstay),
-    # booking window
+    
+    # 2. booking window
     bookingwindow = round(as.numeric(difftime(startdate, orderdate), units = "days"), 0),
-    # daily cost
+    
+    # 3. daily cost
     dailycost = round(totalpaid / lengthofstay, 2),
-    # daily cost per visitor
+    
+    # 4. daily cost per visitor
     dailycostpervisitor = round(dailycost / numberofpeople, 2),
-    # convert sitetype to title case
+  ) %>% 
+  
+  ## clean sitetype ##
+  mutate(
+    # 1. standardize to title case
     sitetype = str_to_title(sitetype),
-    # redefine "Management" sitetype for 2019-2021
+    # 2. redefine "Management" sitetype for 2019-2021
     sitetype = case_when(
       sitetype == "Management" & park == "Agnew Horse Camp" ~ "Equestrian",
       sitetype == "Management" & park %in% c("Almanor",
@@ -282,19 +225,19 @@ usfs_ridb <- raw_ridb2021 %>%
                                              "Summerdale Campground",
                                              "Sycamore Grove",
                                              "Tillie Creek",
-                                             "Whitney Portal") ~ "RV or Tent Only",
+                                             "Whitney Portal") ~ "RV or Tent",
       sitetype == "Management" & park %in% c("Aspen Grove Campground",
                                              "Faucherie",
                                              "Mono Creek") ~ "Tent Only",
       sitetype == "Management" & park == "Grouse Valley" ~ "Shelter",
       TRUE ~ sitetype
     ),
-    # aggregate sitetype
+    # 3. aggregate sitetype
     sitetype = case_when(
-      # day use
+      # a. day use
       lengthofstay == 0 |
         sitetype == "Entry Point" & park == "Cedar Creek Falls" ~ "Day Use",
-      # remote
+      # b. remote
       sitetype %in% c("Group Walk To", 
                       "Walk To", 
                       "Destination Zone") |
@@ -302,40 +245,133 @@ usfs_ridb <- raw_ridb2021 %>%
         sitetype %in% c("Trailhead", 
                         "Entry Point") & park %in% c("Sierra National Forest Wilderness Permits",
                                                      "Inyo National Forest",
-                                                     "Mt. Whitney") ~ "Remote",
-      # shelter
+                                                     "Mt. Whitney",
+                                                     "Desolation Wilderness Permit") ~ "Remote",
+      # c. shelter
       sitetype %in% c("Cabin Electric",
                       "Cabin Nonelectric",
                       "Group Shelter Nonelectric",
                       "Shelter Nonelectric",
                       "Yurt") ~ "Shelter",
-      # water
+      # d. water
       sitetype == "Boat In" |
         sitetype == "Entry Point" & park == "Tuolumne River Permits" ~ "Water",
-      # equestrian
+      # e. equestrian
       sitetype %in% c("Equestrian Nonelectric", 
                       "Group Equestrian") ~ "Equestrian",
-      # rv only
+      # f. rv only
       sitetype %in% c("Group Rv Area Nonelectric", 
                       "Rv Electric",
                       "Rv Nonelectric") ~ "RV Only",
-      # tent only
+      # g. tent only
       sitetype %in% c("Group Tent Only Area Nonelectric",
                       "Tent Only Electric",
                       "Tent Only Nonelectric") |
         sitetype == "Entry Point" & lengthofstay > 0 ~ "Tent Only",
-      # rv or tent
+      # h. rv or tent
       sitetype %in% c("Group Standard Area Nonelectric",
                       "Group Standard Electric",
                       "Group Standard Nonelectric",
-                      "RV or Tent Only",
                       "Standard Nonelectric",
                       "Standard Electric") ~ "RV or Tent",
       TRUE ~ sitetype
     )
   )
 
+
+
+# combine data ----
+
+# save data ----
+write_csv(usfs_ridb, here("data/ridb/clean/usfs_ridb.csv"))
+
+# testing ----
+test_park2018 <- raw_ridb2018 %>% 
+  # add sept_out to rm `_` to match other column names
+  janitor::clean_names(sep_out = "") %>% 
+  # cols of interest
+  select(
+    historicalreservationid,
+    agency,
+    parentlocation,
+    park,
+    sitetype,
+    facilityzip,
+    facilitystate,
+    facilitylongitude,
+    facilitylatitude,
+    customerzip,
+    totalpaid,
+    startdate,
+    enddate,
+    orderdate,
+    numberofpeople
+  ) %>% 
+  # filter for just USFS in CA
+  # NOTEHD: `case_when()` is for 2018 data
+  mutate(facilitystate = case_when(facilitystate == "CA" ~ "California",
+                                   TRUE ~ facilitystate)) %>% 
+  filter(agency == "USFS" & facilitystate == "California") %>% 
+  ## clean customer zip codes ##
+  # 1. extract 5 digit customer zip codes 
+  mutate(customerzip = str_extract(string = customerzip,
+                                   pattern = "[:digit:]{5}")) %>% 
+  # 2. rm invalid customer zip codes
+  filter(!customerzip %in% c("00000", "99999")) %>%
+  # 3. rm NA customer zip codes
+  drop_na(customerzip) %>% 
+  ## clean forestname ##
+  # 1. rename parentlocation to forestname
+  rename(forestname = parentlocation) %>% 
+  mutate(
+    # 2. standardize "National Forest"
+    forestname = str_replace(string = forestname,
+                             pattern = paste(c(
+                               "NF - FS", "NF -FS", "NF- FS",
+                               "NF-FS", "-FS", " - FS"),
+                               collapse = "|"),
+                             replacement = "National Forest"),
+    # 3. standardize to title case
+    forestname = str_to_title(forestname),
+    # 4. fix forestname typos based on spatial data
+    forestname = case_when(
+      forestname == "Columbia R Gorge Nsanational Forest" ~ "Columbia River Gorge National Scenic Area",
+      forestname == "Fremont Winema National Forest" ~ "Fremont-Winema National Forest",
+      forestname == "Lake Tahoe Basin National Forest" ~ "Lake Tahoe Basin Management Unit",
+      forestname == "Mt. Baker-Snoqu National Forest" ~ "Mt. Baker-Snoqualmie National Forest",
+      forestname == "Okanogan - Wenatchee National Forest" ~ "Okanogan-Wenatchee National Forest",
+      forestname == "Rogue River - Siskiyou National Forest" ~ "Rogue River-Siskiyou National Forests",
+      # keep forestname if none of the previous conditions are TRUE
+      TRUE ~ forestname)
+  ) %>% 
+  # 5. rm forests NOT in R5
+  # NOTEHD: forestname var comes from spatial data script
+  filter(forestname %in% forestname_R5) %>% 
+  mutate(park = str_to_title(park),
+         # 2. fix string errors
+         park = str_remove(string = park,
+                           pattern = paste(c("\\(.*", " \\(.*",
+                                             "---.*", " ---.*",
+                                             "-.*", " -.*",
+                                             ",.*"), collapse = "|")),
+         park = str_replace(string = park,
+                            pattern = "@",
+                            replacement = "At"),
+         park = str_replace(string = park,
+                            pattern = "Cg",
+                            replacement = "Campground"),
+         park = str_replace(string = park,
+                            pattern = "&",
+                            replacement = "And"),
+         park = str_replace(string = park,
+                            pattern = paste(c("/", " / "), collapse = "|"),
+                            replacement = " ")) %>% 
+  group_by(forestname, park) %>% 
+  summarize(n = n())
+
+
 # notes ----
+
 ## adding 'Yurt' to park leave it alone for now... ##
 # note that all parks with 'Shelter' sitetype that doesn't say cabin in
 # park name means it was most likely a 'Yurt' sitetype originally
@@ -377,16 +413,6 @@ zipcode2018_test <- usfs_ridb2018 %>%
   # summarize(n = n())
 
 # ~37% of 2019 data (tot 363,714)
-zipcode2019_test <- usfs_ridb %>% 
-  filter(is.na(customerzip) == TRUE)
-
 # ~26% of 2020 data (tot 520,568)
-zipcode2020_test <- usfs_ridb %>% 
-  filter(is.na(customerzip) == TRUE)# %>% 
-  # group_by(forestname) %>% 
-  # summarize(n = n())
-
-# ~23% of 2021 data (tot 607,766)
-zipcode2021_test <- usfs_ridb %>% 
-  filter(is.na(customerzip) == TRUE)
+# ~23% of 2021 data (tot 617,333 obs; tot na 139,645) expect 477,688 obs
 
