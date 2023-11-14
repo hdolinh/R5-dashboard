acb32cf68f28d7d4d7626c23dd884a9d714e8f84
hdolinh <halina@bren.ucsb.edu>
2023-11-13 19:12:48 -0800

    add state info + calculate dist traveled

--- scripts/ridb-processing.R
+++ scripts/ridb-processing.R
@@ -7,6 +7,7 @@ library(vroom)
 library(here)
 library(tidyverse)
 library(janitor)
+library(zipcodeR)
 
 # read in data ----
 # 2018 ridb data
@@ -279,6 +280,101 @@ usfs_ridb <- raw_ridb2021 %>%
   )
 
 
+# add state for each ZIP code ----
+# create df of fips and full state names
+fips_list <- c("01", "02", "04", "05", "06", "08", "09", "10", "11", "12", 
+               "13", "15", "16", "17", "18", "19", "20", "21", "22", "23", 
+               "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", 
+               "34", "35", "36", "37", "38", "39", "40", "41", "42", "44", 
+               "45", "46", "47", "48", "49", "50", "51", "53", "54", "55", 
+               "56", "72")
+
+state_list <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL",
+                    "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME",
+                    "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH",
+                    "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI",
+                    "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI",
+                    "WY", "PR")
+
+states_full_names_list <- c("Alabama", "Alaska", "Arizona", "Arkansas", "California", 
+                            "Colorado", "Connecticut", "Delaware", "District of Columbia",
+                            "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana",
+                            "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+                            "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+                            "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+                            "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+                            "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+                            "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia",
+                            "Washington", "West Virginia", "Wisconsin", "Wyoming", "Puerto Rico")
+
+df_states_fips <- as.data.frame(list(fips = fips_list,
+                                     state = state_list,
+                                     state_full = states_names_list))
+
+
+# loop through state df to get all ZIP codes w/in state
+df_states_zip_codes <- data.frame()
+
+for (i in seq_along(fips_list)){
+  state <- zipcodeR::search_fips(state_fips = fips_list[[i]]) %>% 
+    select(zipcode, state)
+  
+  df_states_zip_codes <- rbind(df_states_zip_codes, state)
+}
+
+# add full state name and fips code to list of all ZIP codes for each state
+df_states_fips_zip_codes <- 
+  left_join(x = df_states_zip_codes,
+            y = df_states_fips,
+            by = "state") %>% 
+  select(-fips) %>% 
+  rename(customerstate = state,
+         customerstatefull = state_full,
+         customerzip = zipcode)
+
+# join RIDB data with state data
+ridb_state <- left_join(
+  usfs_ridb, df_states_fips_zip_codes, by = "customerzip"
+) %>% 
+  relocate(customerstate, .after = customerzip) %>% 
+  relocate(customerstatefull, .after = customerstate)
+
+# calculate distance traveled ----
+# bootstrap geometries and reproject to NAD 83
+df_geometries <- usfs_ridb %>% 
+  st_as_sf(coords = c("facilitylongitude", "facilitylatitude"),
+           crs = 4326) %>% 
+  st_transform(crs = 4269) # using NAD83 because measured in meters
+
+# get centroid of geometries for all US ZIP codes 
+df_zip_centroids_us <- get_acs(geography = "zcta", year = 2018, geometry = TRUE, 
+                               summary_var = "B01001_001",
+                               survey = "acs5",
+                               variables = c(male = "B01001_002")) %>% 
+  select(NAME, geometry) %>% 
+  mutate(zip_code = str_sub(NAME, start = -5, end = -1)) %>% 
+  select(zip_code, geometry) %>% 
+  st_centroid()
+
+# join data and calculate `distance_traveled` variable
+df_joined_geometries <- 
+  left_join(x = df_geometries %>% as.data.frame(),
+            y = df_zip_centroids_us %>% as.data.frame(), 
+            by = c("customerzip" = "zip_code")) %>%
+  st_sf(sf_column_name = 'geometry.x') %>% 
+  mutate(distance_traveled_m = st_distance(x = geometry.x, 
+                                           y = geometry.y,
+                                           by_element = TRUE),
+         distance_traveled_m = as.numeric(distance_traveled_m))
+
+# convert back to data.frame (from sf data.frame), remove geometries
+df_joined <- df_joined_geometries %>% 
+  as.data.frame() %>% 
+  extract(col = geometry.x, 
+          into = c('facility_latitude', 'facility_longitude'), 
+          regex = '\\((.*), (.*)\\)', 
+          convert = TRUE, remove = TRUE) %>% 
+  select(-geometry.y)
 
 # combine data ----
 
